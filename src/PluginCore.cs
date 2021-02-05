223b5d5f18c34cd403c5d659affb92e5a415b743
JustFr33z <justfr33z@gmail.com>
2021-02-05 11:09:17 +0100

    Add existing project files

---
+++ src/PluginCore.cs
@@ -0,0 +1,29 @@
+﻿using Chat2File.Tasks;
+using OQ.MineBot.PluginBase.Base;
+using OQ.MineBot.PluginBase.Base.Plugin;
+using OQ.MineBot.PluginBase.Bot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat2File {
+    [Plugin(2, "Chat2File", "Saves the chat of each bot into a text file")]
+    public class PluginCore : IStartPlugin {
+        public override void OnLoad(int version, int subversion, int buildversion) { }
+
+        public override PluginResponse OnEnable(IBotSettings botSettings) {
+            if (!botSettings.loadChat) {
+                Console.WriteLine("'Load chat' must be enabled.");
+                return new PluginResponse(false, "'Load chat' must be enabled.");
+            }
+
+            return new PluginResponse(true);
+        }
+
+        public override void OnStart() {
+            RegisterTask(new Chat());
+        }
+    }
+}
---
+++ src/Tasks/Chat.cs
@@ -0,0 +1,49 @@
+﻿using OQ.MineBot.PluginBase;
+using OQ.MineBot.PluginBase.Base.Plugin.Tasks;
+using OQ.MineBot.PluginBase.Classes.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Chat2File.Tasks {
+    public class Chat : ITask {
+        private StreamWriter writer;
+        private string latestMessage = String.Empty;
+
+        public override bool Exec() => true;
+
+        public async override Task Start() {
+            CreateLogFolder();
+            writer = new StreamWriter($"{Environment.CurrentDirectory}\\Chat2File\\{Context.Player.GetUsername()}.txt");
+            writer.AutoFlush = true;
+            Context.Events.onChat += OnChat;
+        }
+
+        public async override Task Stop() {
+            Context.Events.onChat -= OnChat;
+            writer.Close();
+        }
+
+        private void OnChat(IBotContext context, IChat chat, byte position) {
+            var message = chat.GetText();
+
+            if (message == latestMessage)
+                return;
+
+            if (String.IsNullOrWhiteSpace(message))
+                return;
+
+            latestMessage = message;
+
+            writer.WriteLine(message);
+        }
+
+        private void CreateLogFolder() {
+            if (!File.Exists($"{Environment.CurrentDirectory}\\Chat2File"))
+                Directory.CreateDirectory($"{Environment.CurrentDirectory}\\Chat2File");
+        }
+    }
+}
